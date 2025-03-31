aa31603d7f402a027418f45f558d6a08a566b53b
kostia_makh <kostiamakh@gmail.com>
2025-03-31 10:33:41 +0300

    Inited kaitara docker-compose

---
+++ config/kaitara/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "🛠 Making migrations wgtauth"
+python3 manage.py makemigrations wgtauth
+python3 manage.py migrate wgtauth
+
+echo "🛠 Making migrations wgtbase"
+python3 manage.py makemigrations wgtbase
+python3 manage.py migrate wgtbase
+
+echo "🛠 Making migrations wgtcontent"
+python3 manage.py makemigrations wgtcontent
+python3 manage.py migrate wgtcontent
+
+echo "🛠 Making migrations kaitara"
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "🎨 Compiling SCSS..."
+python3 manage.py compile-scss
+
+echo "📁 Collecting static files..."
+python3 /srv/www/kaitara/kaitara/manage.py collectstatic --noinput
+
+echo "▶️ Running server..."
+python3 manage.py runserver 0.0.0.0:8000
